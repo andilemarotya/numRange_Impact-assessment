d1860979576b13806948a2d6f6fbf629cbb5c04c
Mandile Andile Marotya <marotyamandile@gmail.com>
2023-05-01 03:23:33 +0200

    Initial commit

---
+++ src/cdListNumbers.java
@@ -0,0 +1,14 @@
+import java.util.Collection;
+
+public class numRange implements numberrangesummarizer.NumberRangeSummarizer {
+
+    @Override
+    public Collection<Integer> collect(String input) {
+        return null;
+    }
+
+    @Override
+    public String summarizeCollection(Collection<Integer> input) {
+        return null;
+    }
+}
