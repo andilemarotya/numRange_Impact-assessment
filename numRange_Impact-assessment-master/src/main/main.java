8340a60edd5cde5bfce4094afe545238e4d8c98f
Mandile Andile Marotya <marotyamandile@gmail.com>
2023-05-03 09:43:30 +0200

    Junit tests

--- numRange_Impact-assessment-master/src/main/main.java
+++ numRange_Impact-assessment-master/src/main/main.java
@@ -1,36 +1,27 @@
 package main;
 
 import java.util.Collection;
-
+import java.util.Iterator;
 import main.numberrangesummarizer.cdListNumbers;
 
 public class main {
-    public static void main(String[] args) {
-        cdListNumbers summarize = new cdListNumbers();
-
-        /* "collect" method tests start here*/
-        String sampleInput = "1,3,6,7,8,12,13,14,15,21,22,23,24,31,32";
-
-        //Testing if the method "collect" returns a List / Arraylist
-        System.out.println(summarize.collect(sampleInput));
-        System.out.println();
-
-        //Testing if the values of the returned Arraylist are really Integers instead of string data types
-        for (Integer num : summarize.collect(sampleInput)
-        ) {
-            System.out.println(num + 2);
-        }
-        System.out.println();
-        /* "collect" method tests end here*/
-
-
-
-        /* "summarizeCollection" method tests start here*/
-
-        //Declaring sample collection from the string sample input
-        Collection<Integer> sampleColl = summarize.collect(sampleInput);
-        System.out.println(summarize.summarizeCollection( sampleColl)); //
-
+    public main() {
     }
 
+    public static void main(String[] args) {
+        cdListNumbers summarize = new cdListNumbers();
+        String sampleInput = "1,3,6,7,8,12,13,14,15,21,22,23,24,31,32";
+        System.out.println(summarize.collect(sampleInput));
+        System.out.println();
+        Iterator var4 = summarize.collect(sampleInput).iterator();
+
+        while(var4.hasNext()) {
+            Integer num = (Integer)var4.next();
+            System.out.println(num + 2);
+        }
+
+        System.out.println();
+        Collection<Integer> sampleColl = summarize.collect(sampleInput);
+        System.out.println(summarize.summarizeCollection(sampleColl));
+    }
 }
